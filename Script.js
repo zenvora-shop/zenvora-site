5b211175a7c02557827760d027f51a9e84f41eef
zenvora-shop <shopzenvora@gmail.com>
2025-07-15 20:01:50 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,5 +2,6 @@ const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobileMenu');
 
 hamburger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('show');
+  mobileMenu.style.display = 
+    mobileMenu.style.display === "flex" ? "none" : "flex";
 });
